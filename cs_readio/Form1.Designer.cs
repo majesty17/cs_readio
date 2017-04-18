cb60093a1b84526dac2286e9108211992f5ca736
majesty17 <a-o@qq.com>
2017-04-18 18:55:11 +0800

    first commit

---
+++ cs_readio/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace cs_readio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_open = new System.Windows.Forms.Button();
+            this.textBox_url = new System.Windows.Forms.TextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_open
+            // 
+            this.button_open.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_open.Location = new System.Drawing.Point(475, 11);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(75, 23);
+            this.button_open.TabIndex = 0;
+            this.button_open.Text = "open";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // textBox_url
+            // 
+            this.textBox_url.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_url.Location = new System.Drawing.Point(13, 13);
+            this.textBox_url.Name = "textBox_url";
+            this.textBox_url.Size = new System.Drawing.Size(456, 21);
+            this.textBox_url.TabIndex = 2;
+            this.textBox_url.Text = "http://stream.abacast.net/playlist/foxnewsradio-foxnewsaac-ibc2.m3u";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(13, 229);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(537, 138);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(13, 41);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(537, 76);
+            this.axWindowsMediaPlayer1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(562, 379);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.textBox_url);
+            this.Controls.Add(this.button_open);
+            this.Name = "Form1";
+            this.Text = "Readio";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.TextBox textBox_url;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+    }
+}
+
---
+++ cs_readio/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace cs_readio
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("winmm.dll", EntryPoint = "mciSendString", CharSet = CharSet.Auto)]
+        public static extern int mciSendString(
+         string lpstrCommand,
+         string lpstrReturnString,
+         int uReturnLength,
+         int hwndCallback
+        );  
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+
+        //打开
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            string url = textBox_url.Text.Trim();
+            axWindowsMediaPlayer1.URL = url;
+            axWindowsMediaPlayer1.Ctlcontrols.play();
+            
+        }
+
+
+        //测试
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+			//用来post
+			HttpWebRequest request;
+			//初始化httpwebrequest对象
+
+            String post_url = "http://vop.baidu.com/server_api?lan=en&cuid=iamreadio_test&token=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+			request=(HttpWebRequest)WebRequest.Create(post_url);
+			request.Method="POST";
+            request.ServicePoint.Expect100Continue = false;
+			request.ContentType="audio/wav;rate=8000";
+			request.UserAgent="Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.65 Safari/537.36";
+			
+
+			//填写post数据
+
+            FileStream fsRead = new FileStream(@"C:\Users\Majesty\Desktop\8000.wav",FileMode.Open);
+            byte[] data=new byte[102400];
+            int data_len = fsRead.Read(data, 0, data.Length);
+            fsRead.Close();
+
+			request.ContentLength=data_len;
+			request.GetRequestStream().Write(data,0,data_len);
+				
+			HttpWebResponse res=(HttpWebResponse)request.GetResponse();
+
+
+			Stream st=res.GetResponseStream();
+			Encoding en=Encoding.GetEncoding("utf-8");
+			StreamReader st_r=new StreamReader(st,en);
+
+
+			char[] read = new char[512];
+			// Reads 256 characters at a time.
+			int count = st_r.Read( read, 0, 512 );
+			StringBuilder sb=new StringBuilder();
+			while ( count > 0 ){
+				// Dumps the 256 characters on a String* and displays the String* to the console.
+				String str = new String( read,0,count );
+				sb.Append(str);
+				count = st_r.Read( read, 0, 512);
+			}
+		
+
+            richTextBox1.AppendText(sb.ToString());
+			
+                        
+		
+        }
+    }
+}
